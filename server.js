cc7e508bd27a719fa8f5cc2b1c7f335f7a362558
Sonechka M <s212288s@gmail.com>
2020-02-05 15:56:08 -0500

    heroku

--- server.js
+++ server.js
@@ -21,44 +21,85 @@ app.get("/api/notes", function(req, res) {
   })
 });
 
-app.post("/api/notes", function(req, res) {
-  console.log(req.body)
-  const newNote = req.body
-  // const parsedNote = JSON.stringify(newNote)
+// app.post("/api/notes", function(req, res) {
+//   console.log(req.body)
+//   const newNote = req.body
+//   // const parsedNote = JSON.stringify(newNote)
 
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-      const parsedArray = JSON.parse(data)
-      console.log(parsedArray)
-      parsedArray.push(newNote)
-      console.log("PUSHED")
-      const stringArray = JSON.stringify(parsedArray)
-      fs.writeFile("./db/db.json", stringArray, "utf-8", (err) => {
-        if (err) throw err
-        console.log("writing")
-      })
-      res.json(parsedArray)
-    })
-})
+//   fs.readFile("./db/db.json", "utf-8", (err, data) => {
+//       const parsedArray = JSON.parse(data)
+//       console.log(parsedArray)
+//       parsedArray.push(newNote)
+//       console.log("PUSHED")
+//       const stringArray = JSON.stringify(parsedArray)
+//       fs.writeFile("./db/db.json", stringArray, "utf-8", (err) => {
+//         if (err) throw err
+//         console.log("writing")
+//       })
+//       res.json(parsedArray)
+//     })
+// })
 
 // DELETE NOTE
 
-app.delete("/api/notes/:id", function(req, res) {
-// get id from note in db
-// if id matches delte
-  const query = req.params
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    const parsedArray = JSON.parse(data)
-    console.log(parsedArray)
+// app.delete("/api/notes/:id", function(req, res) {
+// // get id from note in db
+// // if id matches delte
+//   // const query = req.params
+//   const idToDelete = parseInt(req.params.id);
+//   fs.readFile("./db/db.json", "utf-8", (err, data) => {
+//     const parsedArray = JSON.parse(data)
+//     console.log(parsedArray)
     
+//     idArray = parsedArray.map((note, index) => {
+//       note.id = index
+//     })
+//     console.log("added id", parsedArray)
+//     const filteredArray = parsedArray.filter(note => 
+//       note.id !== parseInt(query.id))
+//     console.log("REMOVED QUERIES", filteredArray)
+//   })
+// })
+
+app.post("/api/notes", function(req, res) {
+  console.log(req.body);
+  const newNote = req.body;
+  // const parsedNote = JSON.stringify(newNote)
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const parsedArray = JSON.parse(data);
+    console.log(parsedArray);
+    parsedArray.push(newNote);
+    console.log("PUSHED");
     idArray = parsedArray.map((note, index) => {
-      note.id = index
-    })
-    console.log("added id", parsedArray)
-    const filteredArray = parsedArray.filter(note => 
-      note.id !== parseInt(query.id))
-    console.log("REMOVED QUERIES", filteredArray)
-  })
-})
+      note.id = index;
+    });
+    const stringArray = JSON.stringify(parsedArray);
+    fs.writeFile("./db/db.json", stringArray, "utf-8", err => {
+      if (err) throw err;
+      console.log("writing");
+    });
+    res.json(parsedArray);
+  });
+});
+
+// DELETE NOTE
+app.delete("/api/notes/:id", function(req, res) {
+  // get id from note in db
+  // if id matches delte
+  const idToDelete = parseInt(req.params.id);
+  let dbJSON = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  console.log(dbJSON);
+  console.log(idToDelete);
+  const filteredArray = dbJSON.filter(note => note.id !== idToDelete);
+  //console.log("query id", query.id);
+  console.log("REMOVED QUERIES", filteredArray);
+  const stringedDB = JSON.stringify(filteredArray);
+  fs.writeFile("./db/db.json", stringedDB, "utf8", (err, data) => {
+    if (err) throw err;
+    console.log("success");
+  });
+  res.json(filteredArray);
+});
 
 
 // ========= HTML ROUTES
